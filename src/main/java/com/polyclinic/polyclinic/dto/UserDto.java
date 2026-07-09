8af025700b19c1e470c5d673fb343b33928378c5
RedGnom <arsik172004@mail.ru>
2026-07-09 23:22:48 +0500

    Переименовал dto пользователя

--- src/main/java/com/polyclinic/polyclinic/dto/UserDto.java
+++ src/main/java/com/polyclinic/polyclinic/dto/UserDto.java
@@ -1,11 +1,13 @@
 package com.polyclinic.polyclinic.dto;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class PatientDto {
     private String email;
 
