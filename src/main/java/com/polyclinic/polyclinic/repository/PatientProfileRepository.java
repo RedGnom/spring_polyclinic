dbf4ee8148a0e240d8213d03098ebda1ef52f00b
RedGnom <arsik172004@mail.ru>
2026-07-10 00:36:26 +0500

    Добавлен метод для загрузки всех полей за один запрос

--- src/main/java/com/polyclinic/polyclinic/repository/PatientProfileRepository.java
+++ src/main/java/com/polyclinic/polyclinic/repository/PatientProfileRepository.java
@@ -7,9 +7,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PatientProfileRepository extends JpaRepository<PatientProfile, Long> {
+
+    // Получаем диагнозы вместе с остальными данными
+    @Query("SELECT DISTINCT p FROM PatientProfile p LEFT JOIN FETCH p.diagnoses WHERE p.userId = :userId")
+    Optional<PatientProfile> findByIdWithDiagnoses(@Param("userId") Long userId);
+
     @Query("SELECT p FROM PatientProfile p JOIN p.diagnoses d WHERE d.id = :diagnoseId")
     List<PatientProfile> findByDiagnoseId(@Param("diagnoseId") Long diagnoseId);
 }
