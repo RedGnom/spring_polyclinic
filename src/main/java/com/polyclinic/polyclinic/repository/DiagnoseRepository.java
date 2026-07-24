d03df7b5dd5e6ce64c22bfb4afe0c9428acd07b4
RedGnom <arsik172004@mail.ru>
2026-07-24 17:56:28 +0500

    Исправлена ошибка с аннотацией и наследованием класса репозиторий jpa

--- src/main/java/com/polyclinic/polyclinic/repository/DiagnoseRepository.java
+++ src/main/java/com/polyclinic/polyclinic/repository/DiagnoseRepository.java
@@ -2,10 +2,13 @@ package com.polyclinic.polyclinic.repository;
 
 import com.polyclinic.polyclinic.entity.Diagnose;
 import com.polyclinic.polyclinic.entity.PatientProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface DiagnoseRepository {
+@Repository
+public interface DiagnoseRepository extends JpaRepository<Diagnose, Long> {
     Optional<Diagnose> findByDiagnosisName(String diagnosisName);
     Optional<Diagnose> findById(Long id);
 }
