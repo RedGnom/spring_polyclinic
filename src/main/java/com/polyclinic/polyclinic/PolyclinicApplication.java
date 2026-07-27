c0c656e99153308beed7ba9e66a05555150e16b9
RedGnom <arsik172004@mail.ru>
2026-07-28 00:29:25 +0500

    Исправлена невозможность загрузки констант из env

--- src/main/java/com/polyclinic/polyclinic/PolyclinicApplication.java
+++ src/main/java/com/polyclinic/polyclinic/PolyclinicApplication.java
@@ -2,12 +2,17 @@ package com.polyclinic.polyclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import me.paulschwarz.springdotenv.spring.DotenvApplicationInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PolyclinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PolyclinicApplication.class, args);
+		new SpringApplicationBuilder(PolyclinicApplication.class)
+				.initializers(new DotenvApplicationInitializer())
+				.run(args);
 	}
 
 }
