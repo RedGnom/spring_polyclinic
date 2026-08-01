60a44f8fa0ce1de9c3cfa1f92110ab02e5d5766f
RedGnom <arsik172004@mail.ru>
2026-08-01 22:15:39 +0500

    Добавлена аннотация для корректного определения даты в dto

--- src/main/java/com/polyclinic/polyclinic/dto/RegisterDto.java
+++ src/main/java/com/polyclinic/polyclinic/dto/RegisterDto.java
@@ -1,11 +1,13 @@
 package com.polyclinic.polyclinic.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -20,6 +22,7 @@ public class RegisterDto {
     private String password;
 
     @NotNull
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate birthDate;
 
     @NotBlank(message = "Имя не может быть пустым")
