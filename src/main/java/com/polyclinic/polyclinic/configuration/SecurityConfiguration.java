2832cf71a288c7b760c61aa2c2c012205d4da9c4
RedGnom <arsik172004@mail.ru>
2026-08-13 01:05:02 +0500

    Добавлен auth provider

--- src/main/java/com/polyclinic/polyclinic/configuration/SecurityConfiguration.java
+++ src/main/java/com/polyclinic/polyclinic/configuration/SecurityConfiguration.java
@@ -1,14 +1,25 @@
 package com.polyclinic.polyclinic.configuration;
 
+import com.polyclinic.polyclinic.security.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration {
+
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -18,5 +29,22 @@ public class SecurityConfiguration {
                 )
                 .csrf(csrf -> csrf.disable());
         return httpSecurity.build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
+        // Встраиваем свой UserDetails
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+
+        // Устанавливаем шифрование
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+
+    }
+    // Шифрование пароля
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/polyclinic/polyclinic/security/CustomUserDetailsService.java
@@ -0,0 +1,4 @@
+package com.polyclinic.polyclinic.security;
+
+public class CustomUserDetailsService {
+}
