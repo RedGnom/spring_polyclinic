b04aba087a96b6337d28fbf4688bd18faf075ba6
RedGnom <arsik172004@mail.ru>
2026-08-21 00:23:56 +0500

    Добавлен метод loadUserByUsername

--- src/main/java/com/polyclinic/polyclinic/security/CustomUserDetailsService.java
+++ src/main/java/com/polyclinic/polyclinic/security/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.polyclinic.polyclinic.security;
 
-public class CustomUserDetailsService {
+import com.polyclinic.polyclinic.entity.User;
+import com.polyclinic.polyclinic.repository.UserRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователя с почтой " + username + " не найден"));
+        // Конвертация entity user в user details
+        return CustomUserDetails.fromEntityUser(user);
+    }
+
 }
