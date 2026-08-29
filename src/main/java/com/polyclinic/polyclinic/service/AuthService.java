3f4a7e19a2c56323754f1ef6546400c4de190235
RedGnom <arsik172004@mail.ru>
2026-08-29 21:23:07 +0500

    Добавил метод авторизации и отдачи токена

--- src/main/java/com/polyclinic/polyclinic/service/AuthService.java
+++ src/main/java/com/polyclinic/polyclinic/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.polyclinic.polyclinic.service;
 
+import com.polyclinic.polyclinic.dto.LoginDto;
+import com.polyclinic.polyclinic.security.CustomUserDetails;
 import com.polyclinic.polyclinic.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +16,19 @@ public class AuthService {
     private final AuthenticationManager authManager;
     private final JwtService jwtService;
 
+    public String login(LoginDto loginDto){
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getEmail(),
+                        loginDto.getPassword()
+                )
+        );
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String token = jwtService.generateToken(userDetails);
+
+        return token;
+    }
 
 }
