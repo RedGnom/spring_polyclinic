0fefc6c66f18ad2314b008692cd63464ce260ed7
RedGnom <arsik172004@mail.ru>
2026-08-30 12:05:06 +0500

    Перенес регистрацию в auth controller

--- src/main/java/com/polyclinic/polyclinic/controller/UserController.java
+++ src/main/java/com/polyclinic/polyclinic/controller/UserController.java
@@ -2,11 +2,13 @@ package com.polyclinic.polyclinic.controller;
 
 import com.polyclinic.polyclinic.dto.RegisterDto;
 import com.polyclinic.polyclinic.dto.UserDto;
+import com.polyclinic.polyclinic.security.CustomUserDetails;
 import com.polyclinic.polyclinic.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,16 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     final private UserService userService;
 
-    @PostMapping("/registration")
-    public ResponseEntity<UserDto> registerUser(@Valid @RequestBody
-                                                RegisterDto registerDto){
-        UserDto userDto = userService.createUser(registerDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
-    }
-
-    @GetMapping
-    public ResponseEntity<UserDto> getUserById(Long userId){
-        UserDto userDto = userService.getUserDto(userId);
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getCurrentUser(
+            @AuthenticationPrincipal CustomUserDetails user
+            ){
+        UserDto userDto = userService.getUserDto(user.getId());
 
         return ResponseEntity.ok().body(userDto);
 
