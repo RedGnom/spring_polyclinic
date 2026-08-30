12672d5e846660c745a2eeda2aad8ba97dfc8d69
RedGnom <arsik172004@mail.ru>
2026-08-30 12:07:02 +0500

    Добавил auth controller

---
+++ src/main/java/com/polyclinic/polyclinic/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.polyclinic.polyclinic.controller;
+
+import com.polyclinic.polyclinic.dto.LoginDto;
+import com.polyclinic.polyclinic.dto.RegisterDto;
+import com.polyclinic.polyclinic.dto.UserDto;
+import com.polyclinic.polyclinic.service.AuthService;
+import com.polyclinic.polyclinic.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody
+                                        LoginDto loginDto){
+        String token = authService.login(loginDto);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<UserDto> registerUser(@Valid @RequestBody
+                                                RegisterDto registerDto){
+        UserDto userDto = userService.createUser(registerDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
+    }
+
+
+}
