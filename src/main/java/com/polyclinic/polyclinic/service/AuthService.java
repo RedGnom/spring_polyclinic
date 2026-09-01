d56626ea2e4c36f7b64b3e96f3a472b68d0835e2
RedGnom <arsik172004@mail.ru>
2026-09-01 16:44:11 +0500

    Добавил метод регистрации в auth service

--- src/main/java/com/polyclinic/polyclinic/service/AuthService.java
+++ src/main/java/com/polyclinic/polyclinic/service/AuthService.java
@@ -1,6 +1,11 @@
 package com.polyclinic.polyclinic.service;
 
+import com.polyclinic.polyclinic.dto.AuthResponse;
 import com.polyclinic.polyclinic.dto.LoginDto;
+import com.polyclinic.polyclinic.dto.RegisterDto;
+import com.polyclinic.polyclinic.dto.UserDto;
+import com.polyclinic.polyclinic.entity.User;
+import com.polyclinic.polyclinic.mapper.UserMapper;
 import com.polyclinic.polyclinic.security.CustomUserDetails;
 import com.polyclinic.polyclinic.security.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final AuthenticationManager authManager;
     private final JwtService jwtService;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
     public String login(LoginDto loginDto){
         Authentication authentication = authManager.authenticate(
@@ -29,6 +36,18 @@ public class AuthService {
         String token = jwtService.generateToken(userDetails);
 
         return token;
+    }
+
+    public AuthResponse register(RegisterDto registerDto){
+        User user = userService.createUser(registerDto);
+
+        CustomUserDetails userDetails = CustomUserDetails.fromEntityUser(user);
+
+        String token = jwtService.generateToken(userDetails);
+
+        UserDto userDto = userMapper.toUserDto(user);
+
+        return new AuthResponse(token, userDto);
     }
 
 }
